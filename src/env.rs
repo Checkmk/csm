2d75d3595a83d92a56070c108b769d1c64cd1d3b
Rick Elrod <rick.elrod@checkmk.com>
2025-11-03 17:00:07 +0100

    Command: csm env info
    Signed-off-by: Rick Elrod <rick.elrod@checkmk.com>

--- src/env.rs
+++ src/env.rs
@@ -111,6 +111,7 @@ pub fn run(config: Config, subcommand: Subcommand) -> ExitCode {
             result.exit_code()
         }
         Subcommand::List => micromamba(&config, vec!["env", "list"]).exit_code(),
+        Subcommand::Info => micromamba(&config, vec!["info"]).exit_code(),
         _ => {
             println!("{:?}", config);
             println!("{:?}", subcommand);
