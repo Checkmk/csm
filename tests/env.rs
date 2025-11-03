64a5df39d2f3d12ad3764e3ae1c8e93d206ca3c3
Rick Elrod <rick.elrod@checkmk.com>
2025-11-03 16:34:17 +0100

    Add test for csm env create
    tests/micromamba-minimal was taken from robotmk-examples unmodified.

--- tests/env.rs
+++ tests/env.rs
@@ -2,16 +2,36 @@ mod common;
 
 use common::Error;
 
-use assert_cmd::cargo::cargo_bin_cmd;
 use predicates::prelude::*;
 
-/// Test `csm env list`
+/// Create an environment with `csm env create`.
+fn csm_env_create(csm: &mut common::Csm, name: &str) -> Result<(), Error> {
+    csm.command()
+        .args(&["env", "create", "-n", name])
+        .current_dir(common::tests_dir().join("micromamba-minimal"))
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Transaction finished"));
+    Ok(())
+}
+
+/// Test `csm env create`
+#[test]
+fn test_csm_env_create() -> Result<(), Error> {
+    let mut csm = common::Csm::new()?;
+    csm_env_create(&mut csm, "test_csm_env_create")
+}
+
+/// Create an environment and then test that it shows in `csm env list`
 #[test]
 fn test_csm_env_list() -> Result<(), Error> {
-    cargo_bin_cmd!()
+    let mut csm = common::Csm::new()?;
+    csm_env_create(&mut csm, "test_csm_env_list")?;
+    csm.command()
         .args(&["env", "list"])
         .assert()
         .success()
-        .stdout(predicate::str::is_match("Name.*Active.*Path")?);
+        .stdout(predicate::str::is_match("Name.*Active.*Path")?)
+        .stdout(predicate::str::contains("test_csm_env_list"));
     Ok(())
 }
