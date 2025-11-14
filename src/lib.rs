5f0f0f2c1c0c7aa7f015e3b1dd11e9a21a982552
Rick Elrod <rick.elrod@checkmk.com>
2025-11-14 12:01:31 +0100

    Move CSMResult + impls to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,29 @@ pub mod init;
 pub mod micromamba;
 pub mod robot;
 pub mod shell;
+
+use log::error;
+use std::fmt;
+use std::process::ExitCode;
+
+pub trait CSMResult {
+    fn finish(&self) -> ExitCode;
+}
+
+impl CSMResult for ExitCode {
+    fn finish(&self) -> Self {
+        *self
+    }
+}
+
+impl<T, E: fmt::Display> CSMResult for Result<T, E> {
+    fn finish(&self) -> ExitCode {
+        match self {
+            Ok(_) => ExitCode::SUCCESS,
+            Err(e) => {
+                error!("{}", e);
+                ExitCode::FAILURE
+            }
+        }
+    }
+}
