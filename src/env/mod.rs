dd2cb8d659e00bf460d3506164509af73ebbd49b
Rick Elrod <rick.elrod@checkmk.com>
2025-11-19 17:16:15 +0100

    Move `csm env activate` to its new home

--- src/env/mod.rs
+++ src/env/mod.rs
@@ -122,42 +122,7 @@ pub fn run(config: Config, subcommand: Subcommand) -> Result<(), ExitCode> {
         Subcommand::List => micromamba(&config, vec!["env", "list"], true).into(),
         Subcommand::Info => micromamba(&config, vec!["info"], true).into(),
         Subcommand::Run(args) => run::run(config, args),
-        Subcommand::Activate(args) => {
-            let Some(shell) = SupportedShell::from_csm_hook() else {
-                error!("Your shell does not appear to have the csm hook enabled");
-                error!("See 'csm init' for information on how to set up the hook");
-                return Err(ExitCode::FAILURE);
-            };
-            let env_name = env_name(args.common.name, &args.common.env_file)?;
-            info!("Activating environment '{}'...", env_name);
-
-            // NOTE: Anything to stdout here is *evaluated by the user's shell*
-            // Use the logging macros instead for user-facing output!
-
-            // Start by adding the mamba prefix bin to PATH
-            let Some(bin_path) = micromamba::bin_path_for_env(&config, &env_name) else {
-                error!(
-                    "Could not determine binary path for environment '{}'",
-                    env_name
-                );
-                return Err(ExitCode::FAILURE);
-            };
-            println!("{}", shell.prepend_path(&bin_path));
-
-            // And a few conda-specific vars
-            let Some(env_path) = micromamba::path_for_env(&config, &env_name) else {
-                error!("Could not determine path for environment '{}'", env_name);
-                return Err(ExitCode::FAILURE);
-            };
-            println!("{}", shell.set_env_var("CONDA_DEFAULT_ENV", &env_name));
-            println!(
-                "{}",
-                shell.set_env_var("CONDA_PREFIX", &env_path.to_string_lossy())
-            );
-            println!("{}", shell.set_env_var("CONDA_SHLVL", "1"));
-
-            Ok(())
-        }
+        Subcommand::Activate(args) => activate::run(config, args),
         Subcommand::Deactivate => {
             let Some(shell) = SupportedShell::from_csm_hook() else {
                 error!("Your shell does not appear to have the csm hook enabled");
